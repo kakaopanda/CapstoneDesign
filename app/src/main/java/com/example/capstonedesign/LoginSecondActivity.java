f2a9a9ee316371817954f97f3f60117e7bd3b44e
JNUSYJ <lukai7501@naver.com>
2022-04-16 20:52:17 +0900

    20220416 login, signup

--- app/src/main/java/com/example/capstonedesign/LoginSecondActivity.java
+++ app/src/main/java/com/example/capstonedesign/LoginSecondActivity.java
@@ -3,9 +3,9 @@ package com.example.capstonedesign;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -13,8 +13,18 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class LoginSecondActivity extends AppCompatActivity {
     ImageView signup_btn, login_btn, another_btn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         applyColors();
@@ -23,6 +33,7 @@ public class LoginSecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_second);
 
+        // 회원가입 버튼
         signup_btn = findViewById(R.id.signup_btn);
         signup_btn.setOnClickListener(new View.OnClickListener(){
 
@@ -33,16 +44,17 @@ public class LoginSecondActivity extends AppCompatActivity {
             }
         });
 
+        // 로그인 버튼
         login_btn = findViewById(R.id.login_btn);
         login_btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                login();
             }
         });
 
+        // 다른 방법으로 로그인 버튼
         another_btn = findViewById(R.id.another_btn);
         another_btn.setOnClickListener(new View.OnClickListener() {
 
@@ -63,5 +75,48 @@ public class LoginSecondActivity extends AppCompatActivity {
     private void applyColors() {
         getWindow().setStatusBarColor(Color.parseColor("#FF1073B4"));
         getWindow().setNavigationBarColor(Color.parseColor("#FF1073B4"));
+    }
+
+    // 로그인 기능
+    private void login() {
+        EditText idText = (EditText)findViewById(R.id.sample_EditText);
+        EditText pwText = (EditText)findViewById(R.id.sample_EditText2);
+        String id = idText.getText().toString();
+        String pw = pwText.getText().toString();
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        Gson gson = new GsonBuilder().setLenient().create();
+        Toast failedToast = Toast.makeText(getApplicationContext(), "LOGIN FAILED", Toast.LENGTH_LONG);
+        Toast serverErrorToast = Toast.makeText(getApplicationContext(), "SERVER ERROR", Toast.LENGTH_LONG);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.35.216:8080/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        LoginService service = retrofit.create(LoginService.class);
+
+        Call<String> call = service.login(id,pw);
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if(response.isSuccessful()) {
+                    String result = response.body();
+                    if(result.equals("SUCCESS")) {
+                        startActivity(intent);
+                        Log.d("Login","onResponse: 성공, 결과:\n"+result.toString());
+                    }
+                    else {
+                        failedToast.show();
+                    }
+                } else {
+                    failedToast.show();
+                    Log.d("Login", "onResponse: 실패");
+                }
+            }
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                serverErrorToast.show();
+                Log.d("TAG", "onFailure " + t.getMessage());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/capstonedesign/SignupFirstActivity.java
+++ app/src/main/java/com/example/capstonedesign/SignupFirstActivity.java
@@ -3,16 +3,28 @@ package com.example.capstonedesign;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class SignupFirstActivity extends AppCompatActivity {
     ImageView submit_btn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         applyColors();
@@ -26,8 +38,16 @@ public class SignupFirstActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SignupSecondActivity.class);
-                startActivity(intent);
+                EditText pwText = (EditText)findViewById(R.id.sample_EditText8);
+                EditText pw2Text = (EditText)findViewById(R.id.sample_EditText3);
+                String pw = pwText.getText().toString();
+                String pw2 = pw2Text.getText().toString();
+                if(pw.equals(pw2)) {
+                    signup();
+                } else{
+                    Toast failedToast = Toast.makeText(getApplicationContext(),"CHECK PASSWORD",Toast.LENGTH_LONG);
+                    failedToast.show();
+                }
             }
         });
     }
@@ -48,6 +68,47 @@ public class SignupFirstActivity extends AppCompatActivity {
     public void onBackPressed() {
         //super.onBackPressed();
     }
-
      */
+
+    public void signup() {
+        EditText idText = (EditText)findViewById(R.id.sample_EditText9);
+        EditText pwText = (EditText)findViewById(R.id.sample_EditText8);
+        EditText pw2Text = (EditText)findViewById(R.id.sample_EditText3);
+        String id = idText.getText().toString();
+        String pw = pwText.getText().toString();
+        String pw2 = pw2Text.getText().toString();
+        Gson gson = new GsonBuilder().setLenient().create();
+        Toast failedToast = Toast.makeText(getApplicationContext(), "SIGNUP FAILED", Toast.LENGTH_LONG);
+        Intent intent = new Intent(getApplicationContext(), SignupSecondActivity.class);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.35.216:8080/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        SignupService service = retrofit.create(SignupService.class);
+
+        Call<String> call = service.signup(id,pw);
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if(response.isSuccessful()) {
+                    String result = response.body();
+                    if(result.equals("SUCCESS")) {
+                        startActivity(intent);
+                    }
+                    else {
+                        failedToast.show();
+                    }
+                    Log.d("Login","onResponse: 성공, 결과:\n"+result.toString());
+                } else {
+                    failedToast.show();
+                    Log.d("Login", "onResponse: 실패");
+                }
+            }
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Log.d("TAG", "onFailure " + t.getMessage());
+            }
+        });
+    }
 }
