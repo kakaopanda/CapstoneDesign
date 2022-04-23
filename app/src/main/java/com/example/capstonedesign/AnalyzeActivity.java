ea6608e0a8cdbfbb77fa1c993c9b037eb50870e3
kakaopanda <kakao_panda@naver.com>
2022-04-23 18:11:52 +0900

    20220423

--- app/src/main/java/com/example/capstonedesign/AnalyzeActivity.java
+++ app/src/main/java/com/example/capstonedesign/AnalyzeActivity.java
@@ -71,27 +71,36 @@ public class AnalyzeActivity extends AppCompatActivity {
         result_name2.setAnimation(opacityAnim);
     }
 
+    // FileProvider의 Content URI를 통해 스크린샷 이미지를 공유한다.
     public void ScreenShot(){
-        View view = getWindow().getDecorView().getRootView();
-        view.setDrawingCacheEnabled(true);  //화면에 뿌릴때 캐시를 사용하게 한다
-        //캐시를 비트맵으로 변환
+        View view = getWindow().getDecorView().getRootView(); // 전체화면을 담은, 현재 Activity의 최상위 View(Root View)를 가져온다.
+        view.setDrawingCacheEnabled(true); // Cache에 해당 View의 이미지를 저장한다.
+        // 현재 Cache에 저장되어있는 이미지를 Bitmap 형태로 변환하여 저장한다.
         Bitmap screenBitmap = Bitmap.createBitmap(view.getDrawingCache());
         try {
+            // 애플리케이션의 임시 Cache 파일을 저장하는 디렉토리를 생성한다.
             File cachePath = new File(getApplicationContext().getCacheDir(), "images");
-            cachePath.mkdirs(); // don't forget to make the directory
-            FileOutputStream stream = new FileOutputStream(cachePath + "/image.png"); // overwrites this image every time
+            cachePath.mkdirs();
+
+            // 이미지 파일을 쓰기 위한 스트림을 선언한 뒤, compress()를 통해 스트림에 Bitmap을 저장한다.
+            FileOutputStream stream = new FileOutputStream(cachePath + "/image.png");
             screenBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
             stream.close();
 
+            // 이미지 파일의 Bitmap이 존재하는 cachePath에 문자열로 경로를 생성하여 File 객체를 생성한다.
             File newFile = new File(cachePath, "image.png");
+            // FileProvider.getUriForFile()에 File 객체를 넘긴 뒤, Content URI를 반환받는다.
             Uri contentUri = FileProvider.getUriForFile(getApplicationContext(),
                     "com.example.capstonedesign.fileprovider", newFile);
 
+            // 공유 인텐트(ACTION_SEND)를 지정한 새로운 Intent를 생성하여, Activity 간 데이터 전송을 수행한다.
             Intent Sharing_intent = new Intent(Intent.ACTION_SEND);
             Sharing_intent.setType("image/png");
+
+            // Extra Data는 인텐트 간 공유할 실제 데이터를 저장하며, Key/Value 쌍의 구조이다.
+            // Key는 이미지 파일의 MIME 형식(전송되는 문서의 형식)인 Intent.EXTRA_STREAM 으로 지정한다.
             Sharing_intent.putExtra(Intent.EXTRA_STREAM, contentUri);
             startActivity(Intent.createChooser(Sharing_intent, "Share image"));
-
         } catch (IOException e) {
             e.printStackTrace();
         }
