94fb506d0fa599791fed4d1c9696f8de4706d724
kakaopanda <kakao_panda@naver.com>
2022-05-10 17:16:18 +0900

    20220510

--- app/src/main/java/com/example/capstonedesign/LoginFirstActivity.java
+++ app/src/main/java/com/example/capstonedesign/LoginFirstActivity.java
@@ -31,7 +31,8 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginFirstActivity extends AppCompatActivity{
     // JAVA Object
-    public static String serverUrl = "http://192.168.35.8:8080/";
+    // [Problem] 외부망에서 내부망에 접근할때, 서버측의 적절한 포트포워딩이 필요하다.
+    public static String serverUrl = "http://1.249.71.81:8080/";
     private Animation scaleAnim, rotateAnim;
     private Toast toast;
     private long backKeyPressedTime = 0;
